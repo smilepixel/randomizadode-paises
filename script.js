4afab31161c45e9c51b595fa7ab274d2481645cb
smilepixel <wesleyxgm@gmail.com>
2025-05-12 13:00:34 -0600

    randomizandole-paises

---
+++ script.js
@@ -0,0 +1,429 @@
+let cero = (0)
+console.log(cero)
+
+
+let boton_1 = document.querySelector("#boton_1")
+    boton_1.addEventListener("click" ,()=>{ 
+
+
+        let bandera_h_L = document.getElementById("bandera_h")
+        let pais_h_L = document.getElementById("pais_h")
+
+
+
+let pais0 = { nombre: "Afganistán", bandera:"🇦🇫", } 
+let pais1 = { nombre: 'Albania', bandera:' 🇦🇱', } 
+let pais2 = { nombre: 'Alemania', bandera:' 🇩🇪', } 
+let pais3 = { nombre: 'Andorra', bandera:' 🇦🇩', } 
+let pais4 = { nombre: 'Angola', bandera:' 🇦🇴', } 
+let pais5 = { nombre: 'Antigua y barbuda', bandera:' 🇦🇬', }
+let pais6 = { nombre: 'Arabia saudita', bandera:' 🇸🇦', } 
+let pais7 = { nombre: 'Argelia', bandera:' 🇩🇿', } 
+let pais8 = { nombre: 'Argentina', bandera:' 🇦🇷', } 
+let pais9 = { nombre: 'Armenia', bandera:' 🇦🇲', } 
+let pais10 = { nombre: 'Australia', bandera:' 🇦🇺', }
+let pais11 = { nombre: 'Austria', bandera:' 🇦🇹', } 
+let pais12 = { nombre: 'Azerbaiyán', bandera:' 🇦🇿', } 
+let pais13 = { nombre: 'Bahamas', bandera:' 🇧🇸', } 
+let pais14 = { nombre: 'Barbados', bandera:' 🇧🇧', } 
+let pais15 = { nombre: 'Baréin', bandera:' 🇧🇭', } 
+let pais16 = { nombre: 'Bélgica', bandera:' 🇧🇪', } 
+let pais17 = { nombre: 'Belice', bandera:' 🇧🇿', }
+let pais18 = { nombre: 'Bielorrusia', bandera:' 🇧🇾', } 
+let pais19 = { nombre: 'Benín', bandera:' 🇧🇯', } 
+let pais20 = { nombre: 'Myanmar', bandera:' 🇲🇲', } 
+let pais21 = { nombre: 'Bolivia', bandera:' 🇧🇴', } 
+let pais22 = { nombre: 'Boznia y Herzegovina', bandera:' 🇧🇦', } 
+let pais23 = { nombre: 'Botsuana', bandera:' 🇧🇼', } 
+let pais24 = { nombre: 'Brunéi', bandera:' 🇧🇳', } 
+let pais25 = { nombre: 'Bulgaria', bandera:' 🇧🇬', } 
+let pais26 = { nombre: 'Burkina faso', bandera:' 🇧🇫', } 
+let pais27 = { nombre: 'Burundi', bandera:' 🇧🇮', } 
+let pais28 = { nombre: 'Bután', bandera:' 🇧🇹', } 
+let pais29 = { nombre: 'Cabo verde', bandera:' 🇨🇻', } 
+let pais30 = { nombre: 'Camboya', bandera:' 🇰🇭', } 
+let pais31 = { nombre: 'Camerún', bandera:' 🇨🇲', }
+let pais32 = { nombre: 'Canadá', bandera:' 🇨🇦', } 
+let pais33 = { nombre: 'Qatar', bandera:' 🇶🇦', } 
+let pais34 = { nombre: 'Chad', bandera:' 🇹🇩', } 
+let pais35 = { nombre: 'República checa', bandera:' 🇨🇿', } 
+let pais36 = { nombre: 'Chile', bandera:' 🇨🇱', } 
+let pais37 = { nombre: 'china', bandera:' 🇨🇳', } 
+let pais38 = { nombre: 'Chipre', bandera:' 🇨🇾', } 
+let pais39 = { nombre: 'Colombia', bandera:' 🇨🇴', } 
+let pais40 = { nombre: 'Congo', bandera:' 🇨🇬', }
+let pais41 = { nombre: 'República democrática del congo', bandera:' 🇨🇩', } 
+let pais42 = { nombre: 'Corea del norte', bandera:' 🇰🇵', } 
+let pais43 = { nombre: 'Costa de marfil', bandera:' 🇨🇮', } 
+let pais44 = { nombre: 'costa rica', bandera:' 🇨🇷', } 
+let pais45 = { nombre: 'Croacia', bandera:' 🇭🇷', }
+let pais46 = { nombre: 'Cuba', bandera:' 🇨🇺', } 
+let pais47 = { nombre: 'Dinamarca', bandera:' 🇩🇰', } 
+let pais48 = { nombre: 'República Dominicana', bandera:' 🇩🇴', } 
+let pais49 = { nombre: 'Ecuador', bandera:' 🇪🇨', } 
+let pais50 = { nombre: 'Egipto', bandera:' 🇪🇬', } 
+let pais51 = { nombre: 'El Salvador', bandera:' 🇸🇻', } 
+let pais52 = { nombre: 'Emiratos árabes Unidos', bandera:' 🇦🇪', } 
+let pais53 = { nombre: 'Eslovaquia', bandera:' 🇸🇰', } 
+let pais54 = { nombre: 'Eslovenia', bandera:' 🇸🇮', } 
+let pais55 = { nombre: 'España', bandera:' 🇪🇸', } 
+let pais56 = { nombre: 'Estados Unidos', bandera:' 🇺🇸', } 
+let pais57 = { nombre: 'Estonia', bandera:' 🇪🇪', } 
+let pais58 = { nombre: 'Etiopía', bandera:' 🇪🇹', } 
+let pais59 = { nombre: 'Filipinas', bandera:' 🇵🇭', } 
+let pais60 = { nombre: 'Finlandia', bandera:' 🇫🇮', } 
+let pais61 = { nombre: 'Francia', bandera:' 🇫🇷', } 
+let pais62 = { nombre: 'Gabón', bandera:' 🇬🇦', }
+let pais63 = { nombre: 'Gambia', bandera:' 🇬🇲', } 
+let pais64 = { nombre: 'Georgia', bandera:' 🇬🇪', } 
+let pais65 = { nombre: 'Ghana', bandera:' 🇬🇭', } 
+let pais66 = { nombre: 'Grecia', bandera:' 🇬🇷', } 
+let pais67 = { nombre: 'Guatemala', bandera:' 🇬🇹', } 
+let pais68 = { nombre: 'Guinea-Bisáu', bandera:' 🇬🇼', } 
+let pais69 = { nombre: 'Guinea Ecuatorial', bandera:' 🇬🇶', } 
+let pais70 = { nombre: 'Haití', bandera:' 🇭🇹', } 
+let pais71 = { nombre: 'Honduras', bandera:' 🇭🇳', } 
+let pais72 = { nombre: 'Hungría', bandera:' 🇭🇺', } 
+let pais73 = { nombre: 'India', bandera:' 🇮🇳', } 
+let pais74 = { nombre: 'Indonesia', bandera:' 🇮🇩', } 
+let pais75 = { nombre: 'Irán', bandera:' 🇮🇷', } 
+let pais76 = { nombre: 'Irlanda', bandera:' 🇮🇪', }
+let pais77 = { nombre: 'Islandia', bandera:' 🇮🇸', } 
+let pais78 = { nombre: 'Israel', bandera:' 🇮🇱', }
+let pais79 = { nombre: 'Italia', bandera:' 🇮🇹', } 
+let pais80 = { nombre: 'Jamaica', bandera:' 🇯🇲', } 
+let pais81 = { nombre: 'Japón', bandera:' 🇯🇵', } 
+let pais82 = { nombre: 'Jordania', bandera:' 🇯🇴', }
+let pais83 = { nombre: 'Kazajistán', bandera:' 🇰🇿', } 
+let pais84 = { nombre: 'Kenia', bandera:' 🇰🇪', } 
+let pais85 = { nombre: 'Kirguistán', bandera:' 🇰🇬', }
+let pais86 = { nombre: 'Kuwait', bandera:' 🇰🇼', }
+let pais87 = { nombre: 'laos', bandera:' 🇱🇦', } 
+let pais88 = { nombre: 'Letonia', bandera:' 🇱🇻', }
+let pais89 = { nombre: 'Líbano', bandera:' 🇱🇧', } 
+let pais90 = { nombre: 'Malí', bandera:' 🇲🇱', } 
+let pais91 = { nombre: 'Libia', bandera:' 🇱🇾', } 
+let pais92 = { nombre: 'Liechtenstein', bandera:' 🇱🇮', } 
+let pais93 = { nombre: 'Lituania', bandera:' 🇱🇹', } 
+let pais94 = { nombre: 'Luxemburgo', bandera:' 🇱🇺', } 
+let pais95 = { nombre: 'Macedonia del norte', bandera:' 🇲🇰', } 
+let pais96 = { nombre: 'Madagascar', bandera:' 🇲🇬', } 
+let pais97 = { nombre: 'Malasia', bandera:' 🇲🇾', } 
+let pais98 = { nombre: 'Malawi', bandera:' 🇲🇼', } 
+let pais99 = { nombre: 'Marruecos', bandera:' 🇲🇦', } 
+let pais100 = { nombre: 'Islas Marshall', bandera:' 🇲🇭', } 
+let pais101 = { nombre: 'Mauritania', bandera:' 🇲🇷', } 
+let pais102 = { nombre: 'México', bandera:' 🇲🇽', }
+let pais103 = { nombre: 'Micronesia', bandera:' 🇫🇲', } 
+let pais104 = { nombre: 'Mónaco', bandera:' 🇲🇨', } 
+let pais105 = { nombre: 'Mongolia', bandera:' 🇲🇳', } 
+let pais106 = { nombre: 'Montenegro', bandera:' 🇲🇪', }
+let pais107 = { nombre: 'Mozambique', bandera:' 🇲🇿', } 
+let pais108 = { nombre: 'Namibia', bandera:' 🇳🇦', } 
+let pais109 = { nombre: 'Nauru', bandera:' 🇳🇷', } 
+let pais110 = { nombre: 'Nepal', bandera:' 🇳🇵', } 
+let pais111 = { nombre: 'Nicaragua', bandera:' 🇳🇮', } 
+let pais112 = { nombre: 'Níger', bandera:' 🇳🇪', } 
+let pais113 = { nombre: 'Nigeria', bandera:' 🇳🇬', } 
+let pais114 = { nombre: 'Noruega', bandera:' 🇳🇴', } 
+let pais115 = { nombre: 'Nueva Zelanda', bandera:' 🇳🇿', } 
+let pais116 = { nombre: 'Países bajos', bandera:' 🇳🇱', } 
+let pais117 = { nombre: 'Pakistán', bandera:' 🇵🇰', } 
+let pais118 = { nombre: 'Palaos', bandera:' 🇵🇼', } 
+let pais119 = { nombre: 'Palestina', bandera:' 🇵🇸', } 
+let pais120 = { nombre: 'Panamá', bandera:' 🇵🇦', } 
+let pais121 = { nombre: 'Papúa nueva Guinea', bandera:' 🇵🇬', } 
+let pais122 = { nombre: 'Paraguay', bandera:' 🇵🇾', } 
+let pais123 = { nombre: 'Perú', bandera:' 🇵🇪', } 
+let pais124 = { nombre: 'Polonia', bandera:' 🇵🇱', } 
+let pais125 = { nombre: 'Portugal', bandera:' 🇵🇹', } 
+let pais126 = { nombre: 'Reino Unido', bandera:' 🇬🇧', } 
+let pais127 = { nombre: 'Ruanda', bandera:' 🇷🇼', } 
+let pais128 = { nombre: 'Rumania', bandera:' 🇷🇴', }
+let pais129 = { nombre: 'Rusia', bandera:' 🇷🇺', }
+let pais130 = { nombre: 'Samoa', bandera:' 🇼🇸', } 
+let pais131 = { nombre: 'san cristobal y nieves', bandera:' 🇰🇳', } 
+let pais132 = { nombre: 'san marino', bandera:' 🇸🇲', } 
+let pais133 = { nombre: 'Santo tome y principe', bandera:' 🇸🇹', } 
+let pais134 = { nombre: 'Senegal', bandera:' 🇸🇳', } 
+let pais135 = { nombre: 'Serbia', bandera:' 🇷🇸', } 
+let pais136 = { nombre: 'Seychelles', bandera:' 🇸🇨', } 
+let pais137 = { nombre: 'Singapur', bandera:' 🇸🇬', } 
+let pais138 = { nombre: 'Siria', bandera:' 🇸🇾', } 
+let pais139 = { nombre: 'sri lanka', bandera:' 🇱🇰', } 
+let pais140 = { nombre: 'Sudáfrica', bandera:' 🇿🇦', } 
+let pais141 = { nombre: 'Sudán', bandera:' 🇸🇩', }
+let pais142 = { nombre: 'Suecia', bandera:' 🇸🇪', } 
+let pais143 = { nombre: 'Suiza', bandera:' 🇨🇭', } 
+let pais144 = { nombre: 'Tailandia', bandera:' 🇹🇭', }
+let pais145 = { nombre: 'Tanzania', bandera:' 🇹🇿', } 
+let pais146 = { nombre: 'Tayikistán', bandera:' 🇹🇯', } 
+let pais147 = { nombre: 'Timor-Leste', bandera:' 🇹🇱', } 
+let pais148 = { nombre: 'Togo', bandera:' 🇹🇬', } 
+let pais149 = { nombre: 'trinidad y Tobago', bandera:' 🇹🇹', } 
+let pais150 = { nombre: 'Túnez', bandera:' 🇹🇳', }
+let pais151 = { nombre: 'Turkmenistán', bandera:' 🇹🇲', } 
+let pais152 = { nombre: 'Turquía', bandera:' 🇹🇷', }
+let pais153 = { nombre: "Tuvalu", bandera:"🇹🇻", } 
+let pais154 = { nombre: "Ucrania", bandera:"🇺🇦", } 
+let pais155 = { nombre: "Uganda", bandera:"🇺🇬", } 
+let pais156 = { nombre: "Uruguay", bandera:"🇺🇾", } 
+let pais157 = { nombre: "Uzbekistán", bandera:"🇺🇿", } 
+let pais158 = { nombre: "ciudad del vaticano", bandera:"🇻🇦", } 
+let pais159 = { nombre: "Venezuela", bandera:"🇻🇪", } 
+let pais160 = { nombre: "Vietnam", bandera:"🇻🇳", } 
+let pais161 = { nombre: "Yemen", bandera:"🇾🇪", } 
+let pais162 = { nombre: "Djibouti", bandera:"🇩🇯", } 
+let pais163 = { nombre: "Zambia", bandera:"🇿🇲", } 
+let pais164 = { nombre: "Liberia", bandera:"🇱🇷", } 
+let pais165 = { nombre: "Nueva Caledonia", bandera:"🇳🇨", } 
+let pais166 = { nombre: "Bangladesh", bandera:"🇧🇩", } 
+let pais167 = { nombre: "Bermuda", bandera:"🇧🇲", } 
+let pais168 = { nombre: "Fiyi", bandera:"🇫🇯", } 
+let pais169 = { nombre: "Montserrat", bandera:"🇲🇸", } 
+let pais170 = { nombre: "República centroafricana", bandera:"🇨🇫", } 
+let pais171 = { nombre: "Anguilla", bandera:"🇦🇮", } 
+let pais172 = { nombre: "Islas vírgenes de los Estados Unidos", bandera:"🇻🇮", } 
+let pais173 = { nombre: "Kosovo", bandera:"🇽🇰", } 
+let pais174 = { nombre: "Kiribati", bandera:"🇰🇮", }
+let pais175 = { nombre: "Mauricio", bandera:"🇲🇺", } 
+let pais176 = { nombre: "Honk kong", bandera:"🇭🇰", } 
+let pais177 = { nombre: "Samoa americana", bandera:"🇦🇸", }
+let pais178 = { nombre: "Albania", bandera:"🇦🇱", } 
+let pais179 = { nombre: "Guam", bandera:"🇬🇺", } 
+let pais180 = { nombre: "Dominica", bandera:"🇩🇲", } 
+let pais181 = { nombre: "Granada", bandera:"🇬🇩", } 
+let pais182 = { nombre: "Surinam", bandera:"🇸🇷", } 
+let pais183 = { nombre: "Santa lucia", bandera:"🇱🇨", } 
+let pais184 = { nombre: "Lesoto", bandera:"🇱🇸", } 
+let pais185 = { nombre: "Islas Cook", bandera:"🇨🇰", } 
+let pais186 = { nombre: "Guadalupe", bandera:"🇬🇵", }
+let pais187 = { nombre: "Gibraltar", bandera:"🇬🇮", } 
+let pais188 = { nombre: "Islas feroe", bandera:"🇫🇴", }
+let pais189 = { nombre: "Groenlandia", bandera:"🇬🇱", } 
+let pais190 = { nombre: "Curazao", bandera:"🇨🇼", } 
+let pais191 = { nombre: "Macao", bandera:"🇲🇴", } 
+let pais192 = { nombre: "Maldivas", bandera:"🇲🇻", } 
+let pais193 = { nombre: "Omán", bandera:"🇴🇲", } 
+let pais194 = { nombre: "Gales", bandera: "🏴󠁧󠁢󠁷󠁬󠁳󠁿" } 
+let pais195 = { nombre: "Escocia", bandera: "🏴󠁧󠁢󠁳󠁣󠁴󠁿", } 
+let pais196 = { nombre: "Tonga", bandera:"🇹🇴", } 
+let pais197 = { nombre: "Taiwán", bandera:"🇹🇼", } 
+let pais198 = { nombre: "Suazilandia", bandera:"🇸🇿", } 
+let pais199 = { nombre: "san Vicente y las Granadinas", bandera:"🇻🇨", } 
+let pais200 = { nombre: "Somalia", bandera:"🇸🇴", } 
+let pais201 = { nombre: "Sierra leona", bandera:"🇸🇱", }
+let pais202 = { nombre: "corea del sur", bandera:"🇰🇷", }
+
+
+
+
+
+
+
+
+
+let data = [ pais0 , pais1 , pais2 , pais3 , pais4 , pais5 , pais6 , pais7 , pais8 , pais9 , pais10 , pais11 , pais12 , pais13 , pais14 , pais15 , pais16 , pais17 , 
+    pais18 , pais19 , pais20 , pais21 , pais22 , pais23 , pais24 , pais25 , pais26 , pais27 , pais28 , pais29 , pais30 , pais31 , pais32 , pais33 , pais34 , pais35 , 
+    pais36 , pais37 , pais38 , pais39 , pais40 , pais41 , pais42 , pais43 , pais44 , pais45 , pais46 , pais47 , pais48 , pais49 , pais50 , pais51 , pais52 , pais53 , 
+    pais54 , pais55 , pais56 , pais57 , pais58 , pais59 , pais60 , pais61 , pais62 , pais63 , pais64 , pais65 , pais66 , pais67 , pais68 , pais69 , pais70 , pais71 , 
+    pais72 , pais73 , pais74 , pais75 , pais76 , pais77 , pais78 , pais79 , pais80 , pais81 , pais82 , pais83 , pais84 , pais85 , pais86 , pais87 , pais88 , pais89 , 
+    pais90 , pais91 ,pais92 , pais93 , pais94 , pais95 , pais96 , pais97 , pais98 , pais99 , pais100 , pais101 , pais102 , pais103 , pais104 , pais105 , pais106 , 
+    pais107 , pais108 , pais109 , pais110 , pais111 , pais112 , pais113 , pais114 , pais115 , pais116 , pais117 , pais118 , pais119 , pais120 , pais121 , pais122 , 
+    pais123 , pais124 , pais125 , pais126 , pais127 , pais128 , pais129 , pais130 , pais131 , pais132 , pais133 , pais134 , pais135 , pais136 , pais137 , pais138 , 
+    pais139 , pais140 , pais141 , pais142 , pais143 , pais144 , pais145 , pais146 , pais147 , pais148 , pais149 , pais150 , pais151 , pais152 , pais153 , pais154 , 
+    pais155 , pais156 , pais157 , pais158 , pais159 , pais160 , pais161 , pais162 , pais163 , pais164 , pais165 , pais166 ,pais167 , pais168 , pais169 , pais170 , 
+    pais171 , pais172 , pais173 , pais174 , pais175 , pais176 , pais177 , pais178 , pais179 , pais180 , pais181 , pais182 , pais183 , pais184 , pais185 , pais186 , 
+    pais187 , pais188 , pais189 , pais190 , pais191 , pais192 , pais193 , pais194 , pais195 , pais196 , pais197 , pais198 , pais199 , pais200 , pais201 ,pais202
+]
+
+let paises_random = Math.floor(Math.random()*data.length);
+
+
+let paises_final = data[paises_random];
+
+
+console.log(paises_final)
+
+let  = [199,172,170,69,149,48,121,158,22,131,95,41]
+let numeros_betado_1 = (199)
+let numeros_betado_2 = (172)
+let numeros_betado_3 = (170)
+let numeros_betado_4 = (69)
+let numeros_betado_5 = (149)
+let numeros_betado_6 = (48)
+let numeros_betado_7 = (121)
+let numeros_betado_8 = (158)
+let numeros_betado_9 = (22)
+let numeros_betado_10 = (131)
+let numeros_betado_11 = (95)
+let numeros_betado_12 = (41)
+
+if( paises_random == numeros_betado_1 ){
+    pais_h_L.style.color = "red";
+    pais_h_L.style.fontSize = "35px";
+    console.log("error")
+}
+
+else if( paises_random == numeros_betado_2 ){
+    pais_h_L.style.color = "red";
+    pais_h_L.style.fontSize = "35px";
+    console.log("error")}
+
+    else if( paises_random == numeros_betado_3 ){
+        pais_h_L.style.color = "red";
+        pais_h_L.style.fontSize = "35px";
+        console.log("error")}
+
+
+        else if( paises_random == numeros_betado_4 ){
+            pais_h_L.style.color = "red";
+            pais_h_L.style.fontSize = "35px";
+            console.log("error")}
+
+
+            else if( paises_random == numeros_betado_5 ){
+                pais_h_L.style.color = "red";
+                pais_h_L.style.fontSize = "35px";
+                console.log("error")}
+
+
+                else if( paises_random == numeros_betado_6 ){
+                    pais_h_L.style.color = "red";
+                    pais_h_L.style.fontSize = "35px";
+                    console.log("error")}
+
+
+                    else if( paises_random == numeros_betado_7 ){
+                        pais_h_L.style.color = "red";
+                        pais_h_L.style.fontSize = "35px";
+                        console.log("error")}
+
+
+                        else if( paises_random == numeros_betado_8 ){
+                            pais_h_L.style.color = "red";
+                            pais_h_L.style.fontSize = "35px";
+                            console.log("error")}
+
+                            else if( paises_random == numeros_betado_9 ){
+                                pais_h_L.style.color = "red";
+                                pais_h_L.style.fontSize = "35px";
+                                console.log("error")}
+
+                                else if( paises_random == numeros_betado_10 ){
+                                    pais_h_L.style.color = "red";
+                                    pais_h_L.style.fontSize = "35px";
+                                    console.log("error")}
+
+                                    else if( paises_random == numeros_betado_11 ){
+                                        pais_h_L.style.color = "red";
+                                        pais_h_L.style.fontSize = "35px";
+                                        console.log("error")}
+
+                                        else if( paises_random == numeros_betado_12 ){
+                                            pais_h_L.style.color = "red";
+                                            pais_h_L.style.fontSize = "35px";
+                                            console.log("error")}
+else pais_h_L.style.fontSize = "70px";pais_h_L.style.color = "white";
+
+
+
+
+
+
+function bandera_h (){
+    let bandera_h = document.querySelector("#bandera_h").innerHTML=paises_final.bandera;
+}
+
+function pais_h (){
+    let pais_h = document.querySelector("#pais_h").innerHTML=paises_final.nombre;
+}
+
+
+
+
+
+
+
+pais_h();
+bandera_h();
+
+
+
+let ceroo = (cero + 1)
+
+
+
+while (ceroo == 1){
+    console.log("true")
+    bandera_h_L.style.fontSize = "330px"
+    bandera_h_L.style.marginBottom = "730px"
+    bandera_h_L.style.marginTop = "-40px"
+    ceroo = ceroo - ceroo
+}
+
+
+
+
+if(ceroo == 1){
+console.log("verdadero")
+}
+
+else console.log("false")
+
+
+
+
+    })
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* let color = () => {
+
+}
+ */
+
+/* function agregador_1(){
+    let agregador_1 = document.querySelectorAll("body").innerHTML += <div class="av"> <h1>pais</h1> </div>
+    
+    }
+    
+    
+    
+        function agregador_2(){
+        let agregador_2 = document.querySelectorAll("body").innerHTML += <div class="av"> <h1>bandera</h1> </div>
+        } 
+        
+agregador_1();
+agregador_2();
+
+    })
+ */
